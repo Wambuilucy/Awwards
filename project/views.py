e119730ad491b9c904eec783cd970483da9f4be5
Wambuilucy <wambuilucie99@gmail.com>
2020-10-26 18:00:36 +0300

    commit

--- project/views.py
+++ project/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employee_list(request):
+    return render(request, "employee_registger/employee_list.html")
+
+def projects(request):
+    return render(request, "project/projects.html")
+
+def employee_delete(request):
+    return
